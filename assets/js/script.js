f4e661bcc3a8b768fc07ffcff42d0d5bd4f2a3f7
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-17 17:39:01 +0200

    delete array

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,13 @@ Bonus
 Abbellire con CSS o Bootstrap
 Inserire un bottone che al click fetch altre 10 mail (sostituendo le altre)*/
 const ulEl = document.querySelector('ul')
-const EmailList = []
+
 for (let i = 0; i < 10; i++) {
 	axios
 		.get('https://flynn.boolean.careers/exercises/api/random/mail')
 
 		.then((response) => {
-			EmailList.push(response.data.response)
-
-			let element = EmailList[i]
+			let element = response.data.response
 			const liEl = document.createElement('li')
 			liEl.textContent = element
 			ulEl.appendChild(liEl)
