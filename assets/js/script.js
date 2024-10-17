107bd49551718c94eff6d832fc484cc9db44331d
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-17 15:02:10 +0200

    add list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
 /*Descrizione
-Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 Bonus
 Abbellire con CSS o Bootstrap
 Inserire un bottone che al click fetch altre 10 mail (sostituendo le altre)*/
 
-axios
-	.get('https://flynn.boolean.careers/exercises/api/random/mail')
+const EmailList = []
+for (let i = 0; i < 10; i++) {
+	axios
+		.get('https://flynn.boolean.careers/exercises/api/random/mail')
 
-	.then((response) => console.log(response.data.response))
+		.then((response) => EmailList.push(response.data.response))
+}
+console.log(EmailList)
 
-	.catch((error) => console.log(error))
+const ulEl = document.querySelector('ul')
+console.log(ulEl)
+for (let i = 0; i < EmailList.length; i++) {
+	let element = EmailList[i]
+	const liEl = document.createElement('li')
+	liEl.textContent = element
+	ulEl.appendChild(liEl)
+	console.log(liEl)
+	console.log(ulEl)
+}
