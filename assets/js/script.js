58ad7d24da59c342a8c4d05594cd205e487bb635
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-17 17:42:53 +0200

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,15 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 Bonus
 Abbellire con CSS o Bootstrap
 Inserire un bottone che al click fetch altre 10 mail (sostituendo le altre)*/
-const ulEl = document.querySelector('ul')
 
+const ulEl = document.querySelector('ul')
 for (let i = 0; i < 10; i++) {
 	axios
 		.get('https://flynn.boolean.careers/exercises/api/random/mail')
 
 		.then((response) => {
-			let element = response.data.response
 			const liEl = document.createElement('li')
-			liEl.textContent = element
+			liEl.textContent = response.data.response
 			ulEl.appendChild(liEl)
 		})
 }
-console.log(EmailList)
-
-console.log(ulEl)
